014b2b94f3029e9b9d68b7611095ca0a3325c399
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-10-12 11:27:29 +0800

    Enhance Gosper's hack to 64-bit

--- gosper_hack.go
+++ gosper_hack.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 )
 
-func calculateHammingWeight(x uint32) uint32 {
-	x = ((x & 0xAAAAAAAA) >> 1) + (x & 0x55555555)
-	x = ((x & 0xCCCCCCCC) >> 2) + (x & 0x33333333)
-	x = ((x & 0xF0F0F0F0) >> 4) + (x & 0x0F0F0F0F)
-	x = ((x & 0xFF00FF00) >> 8) + (x & 0x00FF00FF)
-	x = ((x & 0xFFFF0000) >> 16) + (x & 0x0000FFFF)
+func calculateHammingWeight(x uint64) uint64 {
+	x = ((x & 0xAAAAAAAAAAAAAAAA) >> 1) + (x & 0x5555555555555555)
+	x = ((x & 0xCCCCCCCCCCCCCCCC) >> 2) + (x & 0x3333333333333333)
+	x = ((x & 0xF0F0F0F0F0F0F0F0) >> 4) + (x & 0x0F0F0F0F0F0F0F0F)
+	x = ((x & 0xFF00FF00FF00FF00) >> 8) + (x & 0x00FF00FF00FF00FF)
+	x = ((x & 0xFFFF0000FFFF0000) >> 16) + (x & 0x0000FFFF0000FFFF)
+	x = ((x & 0xFFFFFFFF00000000) >> 32) + (x & 0x00000000FFFFFFFF)
 	return x
 }
 
-func next(x uint32) uint32 {
-	var y uint32
-	var c uint32
+func next(x uint64) uint64 {
+	var y uint64
+	var c uint64
 
 	y = x & -x
 	c = x + y
@@ -25,12 +26,12 @@ func next(x uint32) uint32 {
 }
 
 func main() {
-	var curr uint32
+	var curr uint64
 
 	fmt.Print("Enter first element: ")
 	fmt.Scanf("%d", &curr)
 
-	upper_bound := uint32(0x1000000)
+	upper_bound := uint64(0x100000000000000)
 
 	for ; curr < upper_bound; curr = next(curr) {
 		fmt.Printf("value = %#x (%d), hamming weight = %d\n", curr, curr, calculateHammingWeight(curr))
