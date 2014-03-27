6992a1b85b26d718cc62c056351dea7d43097b8f
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-03-27 23:10:41 +0800

    Use lambda to simplify code

--- gosper/haskell/Main.hs
+++ gosper/haskell/Main.hs
@@ -11,7 +11,5 @@ genList item count list
 main = do
     args <- getArgs
     let
-        item = read $ head args :: Integer
-        count = read $ head $ tail args :: Integer
-        in
-            putStrLn $ show $ genList item count []
+        (item:count:_) = map (\x -> read x :: Integer) args
+    putStrLn $ show $ genList item count []
